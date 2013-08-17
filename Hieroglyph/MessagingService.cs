48cff5c59def909f0a8340c043a9f068df1223ce
Kaz <kaz@kazinteractivemedia.com>
2013-08-17 09:07:52 -0700

    initial commit

---
+++ Hieroglyph/MessagingService.cs
@@ -0,0 +1,57 @@
+﻿using SendGridMail;
+using SendGridMail.Transport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hieroglyph
+{
+    public class MessagingService
+    {
+        private string _sendGridUserName;
+        private string _sendGridApiKey;
+
+        public MessagingService(string sendGridUserName, string sendGridApiKey)
+        {
+            _sendGridUserName = sendGridUserName;
+            _sendGridApiKey = sendGridApiKey;
+        }
+
+        public async Task<bool> SendEmail(List<String> recipients, string subject, string bodyHtml, string fromEmail, string fromName)
+        {
+            bool isSuccess = false;
+
+            // Create the email object first, then add the properties.
+            var myMessage = SendGrid.GetInstance();
+
+            // Add the message properties.
+            myMessage.From = new MailAddress(fromEmail);
+            myMessage.Header.AddTo(recipients);
+            myMessage.AddTo(fromEmail);
+            myMessage.Subject = subject;
+
+            //Add the HTML and Text bodies
+            myMessage.Html = bodyHtml;
+            //myMessage.Text = “Hello World plain text!”;
+
+            // Create network credentials to access your SendGrid account.
+            var username = _sendGridUserName;
+            var pswd = _sendGridApiKey;
+            var credentials = new NetworkCredential(username, pswd);
+
+            // Create a Web transport for sending email.
+            var transportWeb = Web.GetInstance(credentials);
+
+            // Send the email.
+            transportWeb.Deliver(myMessage);
+
+            isSuccess = true;
+
+            return isSuccess;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 Hieroglyph
 ==========
 
-Class library for sending emails through SendGrid
+Simple async class library for sending emails through SendGrid
+
+
+####Use
+    //Create recipients
+    List<String> recipients = new List<String>();
+
+    recipients.Add("recipient1@email.com");
+    recipients.Add("recipient1@email.com");
+
+    //Send email:
+    MessagingService messaging = new MessagingService("UserName", "APIKey");
+    await messaging.SendEmail(recipients, "{subject}", "{body}", "{from@email.com}", "{fromName}");
